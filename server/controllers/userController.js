487fe55ff7f49fcbd2c37f9168c2a9d0f1bebd24
Bonface Osuka <bonyosuks12731@gmail.com>
2025-07-11 12:48:39 +0300

    backendfayaaaaaaaa

---
+++ server/controllers/userController.js
@@ -0,0 +1,60 @@
+import bcrypt from "bcryptjs"
+import { generateToken } from "../lib/utils";
+
+//controller  Sign  up  new  user 
+export const signup  = async (req, res) => {
+    const {fullnName, email, password, bio } =  req.body;
+    try {
+        if(!fullnName || !email || !password || !bio){
+            return res.json({success:false, message:"Missing Details"})
+        }
+        const user = await UserActivation.findOne({email});
+
+        if(user){
+            return res.json({success:false, message :"Account already exists"})
+        }
+
+        const  salt = await bcrypt .genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await user.create({
+            fullnName,
+            email,
+            password: hashedPassword,
+            bio,
+        });
+
+        const token = generateToken(newUser._id)
+        res.json({success: true, userData: newUser, token, message: "Account Created  Successfully"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+//controller  to  login user 
+export const login  =  async(req, res) => {
+    try {
+        const {email, password} =  req.body;
+        const userData = await User.findOne({email});
+        const isPasswordCorrect =  await bcrypt.compare(password, userData.password); 
+
+        if(!isPasswordCorrect){
+            return res.json({success:false, message: "Invalid credentials"});
+        }
+
+        const token = generateToken(userData._id)
+        res.json({success: true, userData, token, message: "Login Successful"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+} 
+
+//Controller to check if  user  is  aunthenticated 
+export const checkAuth =  (req, res)=> {
+    res.json({success:true, user: req.user});
+}
