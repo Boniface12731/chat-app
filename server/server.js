175f637aa050b7b930be5ebb823fb81aeda72abf
Bonface Osuka <bonyosuks12731@gmail.com>
2025-07-12 14:26:13 +0300

    wueh

--- server/server.js
+++ server/server.js
@@ -4,15 +4,45 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/user.js";
-
+import messageRouter from "./routes/messageRoute.js";
+import { Socket, Server } from "socket.io";
+import { use } from "react";
 
 //Create  express  app and  HTTP server
 const app =  express();
 const server =  http.createServer(app)
 
+//initialize  socketio  server  
+export const io =  new Server(server, {
+    cors: {origin: "*"}
+})
+
+//store  online users
+export const userSocketMap = {}; //{userId: socketId}
+
+//socket.io .connectio handler 
+io.on("connection", (socket)=> { 
+    const userId = socket.handshake.query.userId;
+    console.log("User connected ", userId);
+
+    if(userId) userSocketMap[userId] =  socket.id;
+
+    //Emit  online  users  to all connected  clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        
+    }) 
+})
+    
+
 //Middleware set  up  
 app.use(express.json({limit: "4mb"}))
 app.use(cors());
+app.use("/api/messages", messageRouter)
 
 app.use("/api/status", (req, res)=> res.send("Server is Live"))
 app.use("/api/auth", userRouter);
